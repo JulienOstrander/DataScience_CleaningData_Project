ea499b8af8c2a6426c604eb22e76109abe3a355d
JulienOstrander <julien@ostrander-net.de>
2020-08-25 14:40:01 +0200

    Checked in initial version. No content yet.

---
+++ CleaningData.R
@@ -0,0 +1,7 @@
+## ----------------------------------------------------------------------------
+## CleaningData.R
+##
+## This my R script for the peer-graded assignment project in the "Getting and 
+## Cleaning Data" course of the Data-Science Specialization.
+## 
+## ----------------------------------------------------------------------------
